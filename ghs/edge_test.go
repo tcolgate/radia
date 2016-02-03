cae76e9c61cc192f8c6f401e9e601ec87b2bc980
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2016-02-03 20:44:19 +0000

    Test of multiple edges

--- ghs/edge_test.go
+++ ghs/edge_test.go
@@ -10,13 +10,14 @@ func TestEdgeTestMessage1(t *testing.T) {
 	n2 := Node{}
 	Join(&n1, &n2, MakeChanPair)
 
-	if len(n1.Edges) < 1 {
+	if len(n1.Edges) != 1 {
 		t.Fatalf("expected %v edges, got %v", 1, len(n1.Edges))
 	}
-	if len(n2.Edges) < 1 {
+	if len(n2.Edges) != 1 {
 		t.Fatalf("expected %v edges, got %v", 1, len(n2.Edges))
 	}
 
+	sentm := Message{MessageBroadcast}
 	go func() { n1.Edges[0].Send(sentm) }()
 	gotm := n2.Edges[0].Recieve()
 
@@ -30,8 +31,35 @@ func TestEdgeTestMessage1(t *testing.T) {
 func TestEdgeTestMessage2(t *testing.T) {
 	n1 := Node{}
 	n2 := Node{}
-	n1.Join(&n2, MakeChanPair)
-	//m := Message{}
+	n3 := Node{}
+	Join(&n1, &n2, MakeChanPair)
+	Join(&n3, &n2, MakeChanPair)
+
+	if len(n1.Edges) != 1 {
+		t.Fatalf("expected %v edges, got %v", 1, len(n1.Edges))
+	}
+	if len(n2.Edges) != 2 {
+		t.Fatalf("expected %v edges, got %v", 1, len(n2.Edges))
+	}
+
+	if len(n3.Edges) != 1 {
+		t.Fatalf("expected %v edges, got %v", 1, len(n2.Edges))
+	}
+
+	sentm := Message{MessageBroadcast}
+	c := make(chan Message)
+	go func() { c <- n1.Edges[0].Recieve() }()
+	go func() { n3.Edges[0].Send(sentm) }()
+	gotm := n2.Edges[1].Recieve()
+	select {
+	case <-c:
+		t.Fatalf("expected recieve to block")
+	default:
+	}
+
+	if sentm != gotm {
+		t.Fatalf("expected %+v, got %+v", 1, len(n2.Edges))
+	}
 }
 
 func TestEdgesNextEmpty(t *testing.T) {
