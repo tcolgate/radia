381cae1fefbe9f872d317c9a9580e53dc0c3d97a
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2016-02-03 20:48:40 +0000

    Add a doc.go

---
+++ ghs/doc.go
@@ -0,0 +1,5 @@
+// Package ghs is an attempt to implement the Gallagher, Humblet and Spira
+// algorithm for finding Distributed Spanning Trees.
+//
+// https://en.wikipedia.org/wiki/Distributed_minimum_spanning_tree
+package ghs
