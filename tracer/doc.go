f3c43a6f48982db56db3dd780fcec1e5e4e6c135
Tristan Colgate <tcolgate@gmail.com>
2016-02-25 21:52:49 +0000

    TRACER: Add licenses

--- tracer/doc.go
+++ tracer/doc.go
@@ -1,3 +1,20 @@
+// Copyright (c) 2016 Tristan Colgate-McFarlane
+//
+// This file is part of vonq.
+//
+// vonq is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// vonq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with vonq.  If not, see <http://www.gnu.org/licenses/>.
+
 // Package tracer implements a service, and a set of implementations,
 // that recieve and display updates and log message from the nodes involved
 // in a graphalg. The API intentionally mirros the log package
