389074a97a29a03c2f4cdf11a086cc63e21adf02
Tristan Colgate <tcolgate@gmail.com>
2016-02-25 20:56:31 +0000

    GRAPHALG: sketch message protobuf

--- graphalg/graphalg.pb.go
+++ graphalg/graphalg.pb.go
@@ -13,7 +13,7 @@ It has these top-level messages:
 */
 package graphalg
 
-import proto "github.com/golang/protobuf/proto"
+import "github.com/golang/protobuf/proto"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
